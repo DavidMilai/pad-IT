d2d937e8315e4e434d661a914f2787dc33e79c69
DavidMilai <davidmilai67@gmail.com>
2020-12-16 11:02:35 +0300

    home screen minor changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: ListView(
+        body: Column(
           children: [
             Container(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
@@ -44,84 +44,20 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomCard(
-                  height: size.height * 0.19,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(
-                          'Leaves',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Colors.black),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Container(
-                                width: 50,
-                                height: 50,
-                                child: CircularGraph(
-                                  items: 3,
-                                  one: Colors.red,
-                                  three: Colors.green,
-                                  itemOne: 20,
-                                  two: Colors.grey[350],
-                                  itemTwo: 30,
-                                  itemThree: 40,
-                                )),
-                            Hero(
-                              tag: 'taken',
-                              child: TextWithNumber(
-                                text: 'Taken',
-                                numberColor: Colors.red,
-                                number: '10',
-                              ),
-                            ),
-                            Hero(
-                              tag: 'applications',
-                              child: TextWithNumber(
-                                text: 'Applications',
-                                numberColor: Colors.grey[350],
-                                number: '20',
-                              ),
-                            ),
-                            Hero(
-                              tag: 'balance',
-                              child: TextWithNumber(
-                                text: 'Balance',
-                                numberColor: Colors.green,
-                                number: '30',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                    ],
-                  ),
-                ),
-                CustomCard(
+            Expanded(
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                children: [
+                  CustomCard(
                     height: size.height * 0.19,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Padding(
-                          padding: EdgeInsets.symmetric(horizontal: kMyPadding),
+                          padding: EdgeInsets.symmetric(horizontal: 20),
                           child: Text(
-                            'Attendance',
+                            'Siyapei Primary School',
                             style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 16,
@@ -129,113 +65,185 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ),
                         SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Container(
-                                width: 50,
-                                height: 50,
-                                child: CircularGraph(
-                                  items: 3,
-                                  one: Colors.red,
-                                  itemOne: 1,
-                                  two: Colors.yellow,
-                                  itemTwo: 3,
-                                  three: Colors.green,
-                                  itemThree: 10,
-                                )),
-                            TextWithNumber(
-                              text: 'Attendance',
-                              numberColor: Colors.green,
-                              number: '30',
-                            ),
-                            TextWithNumber(
-                              text: 'Late',
-                              numberColor: Colors.yellow,
-                              number: '3',
-                            ),
-                            TextWithNumber(
-                              text: 'Absent',
-                              numberColor: Colors.red,
-                              number: '1',
-                            ),
-                          ],
+                        GestureDetector(
+                          onTap: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                  width: 50,
+                                  height: 50,
+                                  child: CircularGraph(
+                                    items: 3,
+                                    one: Colors.red,
+                                    three: Colors.green,
+                                    itemOne: 20,
+                                    two: Colors.grey[350],
+                                    itemTwo: 30,
+                                    itemThree: 40,
+                                  )),
+                              Hero(
+                                tag: 'taken',
+                                child: TextWithNumber(
+                                  text: 'Girls',
+                                  numberColor: Colors.green,
+                                  number: '10',
+                                ),
+                              ),
+                              Hero(
+                                tag: 'applications',
+                                child: TextWithNumber(
+                                  text: 'Applications',
+                                  numberColor: Colors.grey[350],
+                                  number: '20',
+                                ),
+                              ),
+                              Hero(
+                                tag: 'balance',
+                                child: TextWithNumber(
+                                  text: 'Balance',
+                                  numberColor: Colors.green,
+                                  number: '30',
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                         SizedBox(height: 20),
                       ],
-                    )),
-                CustomCard(
-                    height: size.height * 0.13,
+                    ),
+                  ),
+                  CustomCard(
+                      height: size.height * 0.19,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsets.symmetric(horizontal: kMyPadding),
+                            child: Text(
+                              'Attendance',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.black),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                  width: 50,
+                                  height: 50,
+                                  child: CircularGraph(
+                                    items: 3,
+                                    one: Colors.red,
+                                    itemOne: 1,
+                                    two: Colors.yellow,
+                                    itemTwo: 3,
+                                    three: Colors.green,
+                                    itemThree: 10,
+                                  )),
+                              TextWithNumber(
+                                text: 'Attendance',
+                                numberColor: Colors.green,
+                                number: '30',
+                              ),
+                              TextWithNumber(
+                                text: 'Late',
+                                numberColor: Colors.yellow,
+                                number: '3',
+                              ),
+                              TextWithNumber(
+                                text: 'Absent',
+                                numberColor: Colors.red,
+                                number: '1',
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                        ],
+                      )),
+                  CustomCard(
+                      height: size.height * 0.13,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconWithText(
+                            onTap: () {},
+                            text: 'Calender',
+                            icon: Icons.date_range,
+                            color: Colors.blue[700],
+                          ),
+                          IconWithText(
+                            onTap: () => {},
+                            text: 'Expenses',
+                            icon: Icons.monetization_on,
+                            color: Colors.green,
+                          ),
+                          IconWithText(
+                            text: 'Openings',
+                            icon: Icons.account_box,
+                            color: Colors.yellow,
+                          )
+                        ],
+                      )),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: kMyPadding, vertical: kMyPadding / 2),
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        IconWithText(
+                        Text(
+                          'Latest Arrivals',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.black),
+                        ),
+                        Spacer(),
+                        InkWell(
                           onTap: () {},
-                          text: 'Calender',
-                          icon: Icons.date_range,
-                          color: Colors.blue[700],
-                        ),
-                        IconWithText(
-                          onTap: () => {},
-                          text: 'Expenses',
-                          icon: Icons.monetization_on,
-                          color: Colors.green,
-                        ),
-                        IconWithText(
-                          text: 'Openings',
-                          icon: Icons.account_box,
-                          color: Colors.yellow,
+                          child: Text(
+                            'View All',
+                            style: TextStyle(fontSize: 16, color: Colors.red),
+                          ),
                         )
                       ],
-                    )),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: kMyPadding, vertical: kMyPadding / 2),
-                  child: Row(
-                    children: [
-                      Text(
-                        'Latest Arrivals',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color: Colors.black),
-                      ),
-                      Spacer(),
-                      InkWell(
-                        onTap: () {},
-                        child: Text(
-                          'View All',
-                          style: TextStyle(fontSize: 16, color: Colors.red),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: kMyPadding),
+                    height: size.height * 0.25,
+                    child: ListView(
+                      physics: BouncingScrollPhysics(),
+                      children: [
+                        AnnouncementTile(
+                          announcementsDate: '16 Dec 2020',
+                          announcementTitle: 'Always Kenya',
+                          announcementsDetails: 'Donated 20,000 pads',
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: kMyPadding),
-                  height: size.height * 0.25,
-                  child: ListView(
-                    physics: BouncingScrollPhysics(),
-                    children: [
-                      AnnouncementTile(
-                        announcementsDate: '12 Dec 2020',
-                        announcementTitle: 'Safaricom',
-                        announcementsDetails: 'Donated 20,000 pads',
-                      ),
-                      AnnouncementTile(
-                        announcementsDate: '12 Dec 2020',
-                        announcementTitle: 'Safaricom',
-                        announcementsDetails: 'Donated 20,000 pads',
-                      ),
-                      AnnouncementTile(
-                        announcementsDate: '12 Dec 2020',
-                        announcementTitle: 'Safaricom',
-                        announcementsDetails: 'Donated 20,000 pads',
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                        AnnouncementTile(
+                          announcementsDate: '14 Dec 2020',
+                          announcementTitle: 'Kotex Kenya',
+                          announcementsDetails: 'Donated 50,000 pads',
+                        ),
+                        AnnouncementTile(
+                          announcementsDate: '12 Dec 2020',
+                          announcementTitle: 'Zana Africa',
+                          announcementsDetails: 'Donated 10,000 pads',
+                        ),
+                        AnnouncementTile(
+                          announcementsDate: '5 Dec 2020',
+                          announcementTitle: 'Unilever',
+                          announcementsDetails: 'Donated 15,000 pads',
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
--- lib/widgets/announcement_tile.dart
+++ lib/widgets/announcement_tile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../constants.dart';
+import '../constants.dart';
 
 class AnnouncementTile extends StatelessWidget {
   final String announcementTitle, announcementsDetails, announcementsDate;
